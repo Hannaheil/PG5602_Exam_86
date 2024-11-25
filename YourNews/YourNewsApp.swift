db84a373b540f1cd952c81f3dadce5397c9dc739
Hannaheil <hannah.eilertsen@gmail.com>
2024-11-25 18:17:57 +0100

    Initial Commit

---
+++ YourNews/YourNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourNewsApp.swift
+//  YourNews
+//
+//  Created by Hannah Eilertsen on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct YourNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YourNewsTests/YourNewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  YourNewsTests.swift
+//  YourNewsTests
+//
+//  Created by Hannah Eilertsen on 25/11/2024.
+//
+
+import Testing
+@testable import YourNews
+
+struct YourNewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
